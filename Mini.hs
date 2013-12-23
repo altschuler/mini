2b2b5c67d6e833f922c6f0dae29d6f336789f529
Simon Altschuler <simon@altschuler.dk>
2013-12-23 10:56:36 +0100

    parse simple expressions

--- Mini.hs
+++ Mini.hs
@@ -1,5 +1,6 @@
 module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
 import System.Environment
 
 data LispVal = Atom String
@@ -15,7 +16,32 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+            <|> parseString
+            <|> parseNumber
+
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
