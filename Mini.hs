6c3a73cae421bb1d0d1ad5161252869e5e85f87a
Simon Altschuler <simon@altschuler.dk>
2013-12-27 17:25:21 +0100

    replify

--- Mini.hs
+++ Mini.hs
@@ -4,9 +4,9 @@
 module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
-import Data.List
 import "mtl" Control.Monad.Error
 import System.Environment
+import System.IO
 import Numeric
 import Utils
 
@@ -54,6 +54,31 @@ instance Error LispError where
   strMsg = Default
 
 type ThrowsError = Either LispError
+
+-- repl
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "mini> ") evalAndPrint
+
+-- compiler
 
 extractValue :: ThrowsError a -> a
 extractValue (Right val) = val
@@ -186,7 +211,7 @@ evalCaseClause key val conseq alt = do
   case result of
     Bool True  -> eval conseq
     Bool False -> alt
-    otherwise -> throwError $ TypeMismatch "boolean" result
+    otherwise  -> throwError $ TypeMismatch "boolean" result
 
 evalCase :: LispVal -> [LispVal] -> ThrowsError LispVal
 evalCase _ [List [Atom "else", conseq]] = eval conseq
@@ -355,5 +380,7 @@ trapError action = catchError action (return . show)
 main :: IO ()
 main = do
   args <- getArgs
-  evaled <- return $ liftM show $ (readExpr (args !! 0) >>= eval)
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ args !! 0
+    otherwise -> putStrLn "Give me none or one args, please"
